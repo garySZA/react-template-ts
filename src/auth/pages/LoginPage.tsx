2b2eabb18a99314dfc8c31ac7d2cc41aa5bbfb05
garySZA <garylvlsoliz@gmail.com>
2024-09-24 18:23:29 -0400

    prettier configuration

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -1,25 +1,22 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 export const LoginPage = () => {
     const navigate = useNavigate();
-    
+
     const onLogin = () => {
-        navigate( '/home', {
-            replace: true
-        })
-    }
-    
+        navigate('/home', {
+            replace: true,
+        });
+    };
+
     return (
         <div className="container mt-5">
             <h2>Login</h2>
             <hr />
 
-            <button
-                className="btn btn-primary"
-                onClick={ onLogin }
-            >
+            <button className="btn btn-primary" onClick={onLogin}>
                 Login
             </button>
         </div>
-    )
-}
+    );
+};
